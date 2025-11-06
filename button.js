fffab302923c8ff7dbaa1b8daca4e6f69e162c20
CTXL1029 <tvcuatoi03093@gmail.com>
2025-11-06 18:03:55 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,11 +1,16 @@
 function copy_anime_name() {
-    const opn_1 = "https://ctxl1029.github.io/CTXL-backend/copy_anime_name";
-    navigator.clipboard.writeText(opn_1);
-    window.open(opn_1, "_blank");
+    const copy_anime_name = "https://ctxl1029.github.io/CTXL-backend/copy_anime_name";
+    navigator.clipboard.writeText(copy_anime_name);
+    window.open(copy_anime_name, "_blank");
 }
 
 function contact() {
-    const opn_2 = "https://github.com/CTXL1029/CTXL-backend";
-    navigator.clipboard.writeText(opn_2);
-    window.open(opn_2, "_blank");
+    const phone_num = "0978042208";
+    navigator.clipboard.writeText(phone_num);
+    alert("Đã sao chép số điện thoại:\n"+ phone_num);
+    window.location.href = 'tel:0988123456';
+    setTimeout(() => {
+        close();
+        button.disabled = false;
+    }, 2700);
 }

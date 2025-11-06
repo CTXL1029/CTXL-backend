dafadbb8c1c0e9a697147720e80836c5f9698c88
CTXL1029 <tvcuatoi03093@gmail.com>
2025-11-06 17:51:19 +0700

    Half update of backend

---
+++ button.js
@@ -0,0 +1,11 @@
+function copy_anime_name() {
+    const opn_1 = "https://ctxl1029.github.io/CTXL-backend/copy_anime_name";
+    navigator.clipboard.writeText(opn_1);
+    window.open(opn_1, "_blank");
+}
+
+function contact() {
+    const opn_2 = "https://github.com/CTXL1029/CTXL-backend";
+    navigator.clipboard.writeText(opn_2);
+    window.open(opn_2, "_blank");
+}

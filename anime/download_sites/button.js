1c1a25b58906842e695159c1d48fcfbaa6b975e0
CTXL1029 <tvcuatoi03093@gmail.com>
2026-01-13 09:51:21 +0700

    modified:   anime/download_sites/button.js 	modified:   anime/download_sites/index.html 	modified:   anime/download_sites/sites_1.html 	new file:   anime/download_sites/sites_3.html

--- anime/download_sites/button.js
+++ anime/download_sites/button.js
@@ -1,11 +1,11 @@
 function sites_1() {
-    const sites_1 = "\sites_1.html";
-    navigator.clipboard.writeText(sites_1);
-    window.open(sites_1, "_blank");
+    window.open("\sites_1.html", "_blank");
+}
+
+function sites_3() {
+    window.open("\sites_3.html", "_blank");
 }
 
 function sites_5() {
-    const sites_5 = "\sites_5.html";
-    navigator.clipboard.writeText(sites_5);
-    window.open(sites_5, "_blank");
+    window.open("\sites_5.html", "_blank");
 }

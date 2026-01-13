de27423f878f82cfd6452cfec358fe233c4335fe
CTXL1029 <tvcuatoi03093@gmail.com>
2026-01-13 10:48:01 +0700

    update download sites

--- anime/download_sites/button.js
+++ anime/download_sites/button.js
@@ -2,6 +2,10 @@ function sites_1() {
     window.open("\sites_1.html", "_blank");
 }
 
+function sites_2() {
+    window.open("\sites_2.html", "_blank");
+}
+
 function sites_3() {
     window.open("\sites_3.html", "_blank");
 }

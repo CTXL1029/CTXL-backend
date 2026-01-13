e3f9bf4491050d8b70297dde66db18d6118839bd
CTXL1029 <tvcuatoi03093@gmail.com>
2026-01-13 09:16:56 +0700

    update logic

--- anime/download_sites/button.js
+++ anime/download_sites/button.js
@@ -1,3 +1,9 @@
+function sites_1() {
+    const sites_1 = "\sites_1.html";
+    navigator.clipboard.writeText(sites_1);
+    window.open(sites_1, "_blank");
+}
+
 function sites_5() {
     const sites_5 = "\sites_5.html";
     navigator.clipboard.writeText(sites_5);
--- anime/open.js
+++ anime/open.js
@@ -5,43 +5,43 @@ function download_sites() {
 }
 
 function open_1() {
-    const opn_1 = "\run\copyText_1.html";
+    const opn_1 = "\rrun\\copyText_1.html";
     navigator.clipboard.writeText(opn_1);
     window.open(opn_1, "_blank");
 }
 
 function open_2() {
-    const opn_2 = "\run\copyText_2.html";
+    const opn_2 = "\rrun\\copyText_2.html";
     navigator.clipboard.writeText(opn_2);
     window.open(opn_2, "_blank");
 }
 
 function open_3() {
-    const opn_3 = "\run\copyText_3.html";
+    const opn_3 = "\rrun\\copyText_3.html";
     navigator.clipboard.writeText(opn_3);
     window.open(opn_3, "_blank");
 }
 
 function open_4() {
-    const opn_4 = "\run\copyText_4.html";
+    const opn_4 = "\rrun\\copyText_4.html";
     navigator.clipboard.writeText(opn_4);
     window.open(opn_4, "_blank");
 }
 
 function open_5() {
-    const opn_5 = "\run\copyText_5.html";
+    const opn_5 = "\rrun\\copyText_5.html";
     navigator.clipboard.writeText(opn_5);
     window.open(opn_5, "_blank");
 }
 
 function open_6() {
-    const opn_6 = "\run\copyText_6.html";
+    const opn_6 = "\rrun\\copyText_6.html";
     navigator.clipboard.writeText(opn_6);
     window.open(opn_6, "_blank");
 }
 
 function open_7() {
-    const opn_7 = "\run\copyText_7.html";
+    const opn_7 = "\rrun\\copyText_7.html";
     navigator.clipboard.writeText(opn_7);
     window.open(opn_7, "_blank");
 }

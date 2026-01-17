0620a54da0d86ea76b3a79e33f548ed7968bb682
CTXL1029 <tvcuatoi03093@gmail.com>
2026-01-17 20:36:17 +0700

    Add Frieren SS2 download sites

--- anime/download_sites/button.js
+++ anime/download_sites/button.js
@@ -10,6 +10,10 @@ function sites_3() {
     window.open("\sites_3.html", "_blank");
 }
 
+function sites_4() {
+    window.open("\sites_4.html", "_blank");
+}
+
 function sites_5() {
     window.open("\sites_5.html", "_blank");
 }

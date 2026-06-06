ac66d777e0c948fc5e80cf42eafef35b53f82624
Thanh Cong Tran <tvcuatoi03093@gmail.com>
2026-06-06 14:48:39 +0700

    idk

--- button.js
+++ button.js
@@ -1,17 +1,23 @@
 function anime() {
-    const anime = "\anime";
-    navigator.clipboard.writeText(anime);
-    window.open(anime, "_blank");
+  const anime = "\anime";
+  navigator.clipboard.writeText(anime);
+  window.open(anime, "_blank");
 }
 
 function contact() {
-    const contact = "\contact";
-    navigator.clipboard.writeText(contact);
-    window.open(contact, "_blank");
+  const contact = "\contact";
+  navigator.clipboard.writeText(contact);
+  window.open(contact, "_blank");
 }
 
 function package_tracker() {
-    const package_tracker = "\package_tracker";
-    navigator.clipboard.writeText(package_tracker);
-    window.open(package_tracker, "_blank");
+  const package_tracker = "\package_tracker";
+  navigator.clipboard.writeText(package_tracker);
+  window.open(package_tracker, "_blank");
+}
+
+function gate_light_ctrl() {
+  const gate_light_ctrl = "\gate_light_ctrl";
+  navigator.clipboard.writeText(gate_light_ctrl);
+  window.open(gate_light_ctrl, "_blank");
 }
